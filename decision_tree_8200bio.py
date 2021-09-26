bce365097fd10b6bafd84971ea181ebe4dbfe661
Roi <roi2307@gmail.com>
2021-09-26 16:09:43 +0300

    commments

--- decision_tree_8200bio.py
+++ decision_tree_8200bio.py
@@ -191,6 +191,7 @@ class DecisionTreeTrainer:
         # for str in ['diagnosis', 'topography', 'location.coverage', 'size', 'shape', 'pain.pain_type', 'gender', 'lossOfHair.type', 'quantity']:
         #     data[str] = LabelEncoder().fit_transform(data[str])
 
+        # catgorial data encoding
         for str in num_col:
             data[str] = LabelEncoder().fit_transform(data[str])
 
@@ -217,16 +218,63 @@ class DecisionTreeTrainer:
         X = data.drop('diagnosis', axis=1)
         y = data['diagnosis']
 
-        # train model to assess feature importance
-        model = tree.DecisionTreeClassifier(random_state=rng)
-        model.fit(X, self.OneHot(y))
-        dfscores = pd.DataFrame(model.feature_importances_)
-        dfcolumns = pd.DataFrame(X.columns)
-        featureScores = pd.concat([dfcolumns, dfscores], axis=1)
-        featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
+        # choose inly imprtent columns
+        X = X[['pain.pain_type', 'location.coverage', 'age', 'duration.days',       
+       'quantity', 'topography', 'size', 'is_color_condition_red',
+       'is_color_condition_brown', 'itch', 'swelling',
+       'is_primary_locations_leg', 'lossOfHair.exist', 'is_texture_scales', 
+       'is_color_condition_white', 'shape', 'gender',
+       'is_primary_locations_arm', 'is_texture_exfoliation',
+       'is_texture_not_sure', 'is_primary_locations_head', 'is_texture_dry',
+       'is_secondary_locations_neck', 'is_color_condition_normal',
+       'is_primary_locations_back', 'vesicle', 'is_primary_locations_chest',
+       'pain.is_pain', 'is_texture_cracks',
+       'is_secondary_locations_upper_arm_internal',
+       'is_secondary_locations_foot_external', 'is_secondary_locations_toes',
+       'is_color_condition_black', 'is_secondary_locations_nose',
+       'is_secondary_locations_cheeks', 'is_color_condition_purple',
+       'is_secondary_locations_shin_external',
+       'is_secondary_locations_abdomen',
+       'is_secondary_locations_upper_arm_external',
+       'is_secondary_locations_fingers', 'is_texture_wet',
+       'is_secondary_locations_knee', 'is_secondary_locations_navel',
+       'is_secondary_locations_forehead', 'crater',
+       'is_secondary_locations_perioral', 'is_color_condition_blue',
+       'lossOfHair.type', 'is_primary_locations_groin_area',
+       'is_secondary_locations_ear', 'is_nailsdiseases_onychomycosis',
+       'is_secondary_locations_thigh_external', 'is_secondary_locations_chin',
+       'is_secondary_locations_buttock', 'is_secondary_locations_eye',
+       'is_secondary_locations_elbow_pit', 'is_secondary_locations_scrotum',
+       'is_nailsdiseases_pitting', 'is_secondary_locations_kneepit',
+       'is_secondary_locations_back',
+       'is_nailsdiseases_koilonychia__spooning_',
+       'is_secondary_locations_eyelid', 'is_secondary_locations_chest',
+       'is_secondary_locations_hand_internal', 'is_secondary_locations_penis',
+       'is_secondary_locations_scalp', 'is_secondary_locations_thigh_internal',
+       'is_secondary_locations_wrist_internal', 'pus',
+       'is_nailsdiseases_onycholysis', 'bleeding', 'temperature',
+       'duration.from_birth', 'is_texture_rough', 'is_texture_smooth',
+       'is_color_condition_green', 'is_color_condition_grey',
+       'is_color_condition_yellow', 'is_nailsdiseases_beau_s_lines',
+       'is_nailsdiseases_clubbing',
+       'is_nailsdiseases_leukonychia__white_spots_',
+       'is_nailsdiseases_mees__lines', 'is_nailsdiseases_terry_s_nails',
+       'is_nailsdiseases_yellow_nail_syndrome', 'is_primary_locations_buttock',
+       'is_secondary_locations_armpit', 'is_secondary_locations_elbow',
+       'is_secondary_locations_eyebrow']]
 
-        feture_n = 88
-        X = X[featureScores.nlargest(feture_n, 'Score')['Specs']]
+
+        # train model to assess feature importance code
+        # model = tree.DecisionTreeClassifier(random_state=rng)
+        # model.fit(X, self.OneHot(y))
+        # dfscores = pd.DataFrame(model.feature_importances_)
+        # dfcolumns = pd.DataFrame(X.columns)
+        # featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+        # featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
+
+        # feture_n = 88
+        # X = X[featureScores.nlargest(feture_n, 'Score')['Specs']]
+       
         t = pd.concat([X,y],axis=1)
         return t
 
@@ -270,12 +318,14 @@ def main():
     if do_train:
         data = the_tree.load_training_data()
         #data = pd.get_dummies(data, columns=list(set(num_col) & set(data.columns)))
+
         # split data into train & test groups
         inputs_train, inputs_test, outputs_train, outputs_test = sklearn.model_selection.train_test_split(data.drop('diagnosis',axis=1), data['diagnosis'],
                                                                                                           test_size=0.1,
                                                                                                           random_state=rng,
                                                                                                           stratify=data['diagnosis'])
         train = pd.concat([inputs_train,outputs_train],axis=1)
+        
         # training data agmuantion
         np.random.seed(32)
         for d in train['diagnosis'].value_counts().index:
@@ -301,8 +351,7 @@ def main():
         # Lastly, print test accuracy
         test_accuracy_percent = the_tree.evaluate(
             test_data.drop('diagnosis', axis=1), test_data['diagnosis'])
-        print("Test accuracy: {:.2f}%".format(
-            test_accuracy_percent))  # in [0-100], eg 99.99%
+        print("Test accuracy: {:.2f}%".format(test_accuracy_percent))  # in [0-100], eg 99.99%
 
 
 ################################################################################
