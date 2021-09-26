412372c2d017420ae530b4c459423ddc45a6cad7
omerugi <omerugi@users.noreply.github.com>
2021-09-26 13:18:26 +0300

    New Encoders

--- decision_tree_8200bio.py
+++ decision_tree_8200bio.py
@@ -22,17 +22,132 @@ import argparse
 import os
 import numpy as np
 
-from sklearn.preprocessing import LabelBinarizer, MinMaxScaler, MultiLabelBinarizer, LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelBinarizer, MinMaxScaler, MultiLabelBinarizer, LabelEncoder, OneHotEncoder, OrdinalEncoder
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.impute import KNNImputer
 from sklearn.feature_selection import SelectKBest, chi2
 
-
-#from ctgan import CTGANSynthesizer
-#from torch._C import dtype
+# from ctgan import CTGANSynthesizer
+# from torch._C import dtype
 # from torch._C import int64
 
-rng =  np.random.RandomState(0)
+rng = np.random.RandomState(0)
+num_col = ["topography"
+            , "location.coverage"
+            , "size"
+            , "shape"
+            , "pain.pain_type"
+            , "gender"
+            , "lossOfHair.type"
+            , "quantity"
+            , "duration.days"
+            , "temperature"
+            , "age"
+            , "swelling"
+            , "bleeding"
+            , "is_secondary_locations_lips"
+            , "is_secondary_locations_kneepit"
+            , "is_secondary_locations_knee"
+            , "is_secondary_locations_hand_internal"
+            , "is_secondary_locations_hand_external"
+            , "is_secondary_locations_hand"
+            , "is_secondary_locations_groins"
+            , "is_secondary_locations_gingiva"
+            , "is_secondary_locations_forehead"
+            , "is_secondary_locations_forearm_internal"
+            , "is_secondary_locations_forearm_external"
+            , "is_secondary_locations_foot_internal"
+            , "is_secondary_locations_foot_external"
+            , "is_secondary_locations_foot"
+            , "is_secondary_locations_fingers"
+            , "is_secondary_locations_eyelid"
+            , "is_secondary_locations_eyebrow"
+            , "is_secondary_locations_eye"
+            , "is_secondary_locations_elbow_pit"
+            , "is_secondary_locations_elbow"
+            , "is_secondary_locations_ear"
+            , "is_secondary_locations_nails"
+            , "is_secondary_locations_navel"
+            , "is_secondary_locations_neck"
+            , "is_secondary_locations_thigh_internal"
+            , "is_secondary_locations_wrist_internal"
+            , "is_secondary_locations_wrist_external"
+            , "is_secondary_locations_vaginal_region"
+            , "is_secondary_locations_upper_arm_internal"
+            , "is_secondary_locations_upper_arm_external"
+            , "is_secondary_locations_tongue"
+            , "is_secondary_locations_toes_internal"
+            , "is_secondary_locations_toes_external"
+            , "is_secondary_locations_toes"
+            , "is_secondary_locations_thigh_external"
+            , "is_secondary_locations_nose"
+            , "is_secondary_locations_shoulders"
+            , "is_secondary_locations_shin_internal"
+            , "is_secondary_locations_shin_external"
+            , "is_secondary_locations_scrotum"
+            , "is_secondary_locations_scalp"
+            , "is_secondary_locations_rear_neck"
+            , "is_secondary_locations_pubes"
+            , "is_secondary_locations_perioral"
+            , "is_secondary_locations_penis"
+            , "is_secondary_locations_chin"
+            , "is_secondary_locations_chest"
+            , "is_secondary_locations_cheeks"
+            , "is_texture_rough"
+            , "is_color_condition_normal"
+            , "is_color_condition_grey"
+            , "is_color_condition_green"
+            , "is_color_condition_brown"
+            , "is_color_condition_blue"
+            , "is_color_condition_black"
+            , "is_texture_wet"
+            , "is_texture_smooth"
+            , "is_texture_scales"
+            , "is_texture_not_sure"
+            , "is_color_condition_red"
+            , "is_texture_exfoliation"
+            , "is_texture_dry"
+            , "is_texture_cracks"
+            , "itch"
+            , "crater"
+            , "lossOfHair.exist"
+            , "vesicle"
+            , "pain.is_pain"
+            , "duration.from_birth"
+            , "is_color_condition_purple"
+            , "is_color_condition_white"
+            , "is_secondary_locations_buttock"
+            , "is_primary_locations_arm"
+            , "is_secondary_locations_back"
+            , "is_secondary_locations_armpit"
+            , "is_secondary_locations_abdomen"
+            , "pus"
+            , "is_primary_locations_head"
+            , "is_primary_locations_groin_area"
+            , "is_primary_locations_chest"
+            , "is_primary_locations_buttock"
+            , "is_primary_locations_back"
+            , "is_nailsdiseases_yellow_nail_syndrome"
+            , "is_color_condition_yellow"
+            , "is_nailsdiseases_terry_s_nails"
+            , "is_nailsdiseases_pitting"
+            , "is_nailsdiseases_onychomycosis"
+            , "is_nailsdiseases_onycholysis"
+            , "is_nailsdiseases_mees__lines"
+            , "is_nailsdiseases_leukonychia__white_spots_"
+            , "is_nailsdiseases_koilonychia__spooning_"
+            , "is_nailsdiseases_clubbing"
+            , "is_nailsdiseases_beau_s_lines"
+            , "is_primary_locations_leg"
+                    ]
+ord_col = ["location.coverage"
+    , "size"
+    , "shape"
+    , "pain.pain_type"
+    , "gender"
+    , "lossOfHair.type"
+    , "quantity"
+           ]
 
 class DecisionTreeTrainer:
     """ A class for training a decision tree on DermaDetect data """
@@ -43,7 +158,7 @@ class DecisionTreeTrainer:
         self.main_dir = os.path.dirname(os.path.abspath(__file__))
 
         # TODO Fine-tune the decision tree classifier's parameters here
-        self.model = tree.DecisionTreeClassifier(random_state=rng,splitter='best')
+        self.model = tree.DecisionTreeClassifier(random_state=rng, splitter='best')
 
     def load_data(self, input_file):
         data = pd.read_csv(input_file)
@@ -67,9 +182,8 @@ class DecisionTreeTrainer:
         self.model = model
 
     def preprocess_data(self, data):
-        
         original = data.copy()
-        
+
         y_mask = data['pain.pain_type'].isna()
         index_train_pain = np.asarray(y_mask[y_mask == False].index)
         index_test_pain = np.asarray(y_mask[y_mask == True].index)
@@ -81,85 +195,81 @@ class DecisionTreeTrainer:
         mask = data.isnull()
         data = data.fillna('unknown')
 
-        for str in ['diagnosis', 'topography', 'location.coverage', 'size', 'shape', 'pain.pain_type', 'gender', 'lossOfHair.type', 'quantity']:
+        ## Working ver - 43%
+        # for str in ['diagnosis', 'topography', 'location.coverage', 'size', 'shape', 'pain.pain_type', 'gender', 'lossOfHair.type', 'quantity']:
+        #     data[str] = LabelEncoder().fit_transform(data[str])
+
+        for str in num_col:
             data[str] = LabelEncoder().fit_transform(data[str])
 
+        for str in ord_col:
+            data[str] = OrdinalEncoder().fit_transform(data[str].to_numpy().reshape(-1,1))
+
+        data["diagnosis"] = LabelEncoder().fit_transform(data["diagnosis"])
+
+        # Return original Nan values
         data = data.where(~mask, original)
 
-        # imputer = KNNImputer(n_neighbors=18,weights='distance')
-        # data = pd.DataFrame(imputer.fit_transform(data))
-        # data.columns = original.columns
+        imputer = KNNImputer(n_neighbors=18, weights='distance')
+        data = pd.DataFrame(imputer.fit_transform(data))
+        data.columns = original.columns
 
         # scaling the numric data
         scaler = MinMaxScaler()
-        data[['duration.days', 'temperature', 'age']] = scaler.fit_transform(data[['duration.days', 'temperature', 'age']])
-        data.replace({False: 0, True: 1}, inplace=True)
+        data[['duration.days', 'temperature', 'age']] = scaler.fit_transform(
+            data[['duration.days', 'temperature', 'age']])
 
-        train = pd.DataFrame(data.to_numpy()[index_train, :], columns=data.columns)
-        x_train = train.drop('diagnosis', axis=1).drop('location.coverage', axis=1).drop('pain.pain_type',axis=1)
-        y_train = train['location.coverage']
-        test = pd.DataFrame(data.to_numpy()[index_test, :], columns=data.columns)
-        x_test = test.drop('diagnosis', axis=1).drop('location.coverage', axis=1).drop('pain.pain_type',axis=1)
-
-
-
-        from sklearn.ensemble import RandomForestClassifier
-        fill_model = RandomForestClassifier(random_state=rng)
-        fill_model.fit(x_train, y_train)
-        y_test = fill_model.predict(x_test)
-        data.loc[index_test,'location.coverage'] = y_test
-
-        # pain completion 
-        train = pd.DataFrame(data.to_numpy()[index_train_pain, :], columns=data.columns)
-        x_train = train.drop('diagnosis', axis=1).drop('pain.pain_type',axis=1)
-        y_train = train['pain.pain_type']
-        test = pd.DataFrame(data.to_numpy()[index_test_pain, :], columns=data.columns)
-        x_test = test.drop('diagnosis', axis=1).drop('pain.pain_type',axis=1)
-
-        fill_model = RandomForestClassifier(random_state=rng)
-        fill_model.fit(x_train, y_train)
-        y_test = fill_model.predict(x_test)
-        data.loc[index_test_pain,'pain.pain_type'] = y_test
+        ## Working ver - 43%
+        #data.replace({False: 0, True: 1}, inplace=True)
 
         # place holders
-        X = data.drop('diagnosis',axis=1)
+        X = data.drop('diagnosis', axis=1)
         y = data['diagnosis']
 
         # split data into train & test groups
-        inputs_train, inputs_test, outputs_train, outputs_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1, random_state=rng, stratify=y)
+        inputs_train, inputs_test, outputs_train, outputs_test = sklearn.model_selection.train_test_split(X, y,
+                                                                                                          test_size=0.1,
+                                                                                                          random_state=rng,
+                                                                                                          stratify=y)
 
-        train = pd.concat([inputs_train, outputs_train],axis=1)
+        train = pd.concat([inputs_train, outputs_train], axis=1)
         train.columns = data.columns
-        test = pd.concat([inputs_test, outputs_test],axis=1)
+        test = pd.concat([inputs_test, outputs_test], axis=1)
         test.columns = data.columns
 
         # train model to assess feature importance
         model = tree.DecisionTreeClassifier(random_state=rng)
-        model.fit(inputs_train,outputs_train)
+        model.fit(inputs_train, self.OneHot(outputs_train))
         dfscores = pd.DataFrame(model.feature_importances_)
         dfcolumns = pd.DataFrame(inputs_train.columns)
         featureScores = pd.concat([dfcolumns, dfscores], axis=1)
         featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
-        
+
         feture_n = 88
-        #featureScores.nlargest(feture_n, 'Score')['Specs'].to_csv("88feacture.csv")
+        # featureScores.nlargest(feture_n, 'Score')['Specs'].to_csv("88feacture.csv")
         inputs_train = inputs_train[featureScores.nlargest(feture_n, 'Score')['Specs']]
         inputs_test = inputs_test[featureScores.nlargest(feture_n, 'Score')['Specs']]
-        
-        train = pd.concat([inputs_train,outputs_train],axis=1)
-        test = pd.concat([inputs_test,outputs_test],axis=1)
-        return train,test
+
+        train = pd.concat([inputs_train, outputs_train], axis=1)
+        test = pd.concat([inputs_test, outputs_test], axis=1)
+        return train, test
 
     def train(self, inputs, outputs):
-        self.model.fit(inputs, outputs)
+        inputs = pd.get_dummies(inputs, columns=num_col)
+        self.model.fit(inputs, self.OneHot(outputs))
         self.save_model()
 
     def evaluate(self, inputs, outputs):
         y_pred = self.model.predict(inputs)
-        #print(classification_report(outputs, y_pred))
-
+        # print(classification_report(outputs, y_pred))
         # TODO: accurately compute the amount of correct predictions over the inputs size in %
-        return sklearn.metrics.accuracy_score(outputs, y_pred)*100
+        return sklearn.metrics.accuracy_score(self.OneHot(outputs), y_pred) * 100
+
+    def OneHot(self, data):
+        ohe = OneHotEncoder()
+        y = data.to_numpy().reshape(-1, 1)
+        y_hot = ohe.fit_transform(y).toarray()
+        return y_hot
 
 
 ################################################################################
@@ -171,7 +281,8 @@ def get_cmd_args():
                       default='', help='Path to the test dataset')
     return args.parse_args()
 
-#from imblearn.over_sampling import SMOTENC
+
+# from imblearn.over_sampling import SMOTENC
 ################################################################################
 def main():
     args = get_cmd_args()
@@ -181,7 +292,7 @@ def main():
 
     if do_train:
         train, test = the_tree.load_training_data()
-        
+
         # training data agmuantion
         np.random.seed(32)
         for d in train['diagnosis'].value_counts().index:
@@ -193,12 +304,12 @@ def main():
             new_data["diagnosis"] = np.full(10, d)
             train = pd.concat([train, pd.DataFrame(new_data)])
 
-        #train and save model
-        the_tree.train(train.drop('diagnosis',axis=1), train['diagnosis'])
+        # train and save model
+        the_tree.train(train.drop('diagnosis', axis=1), train['diagnosis'])
 
         print(train.shape)
-        #evaluate on test group
-        print('accuracy', the_tree.evaluate(test.drop('diagnosis',axis=1), test['diagnosis']))
+        # evaluate on test group
+        print('accuracy', the_tree.evaluate(test.drop('diagnosis', axis=1), test['diagnosis']))
 
     else:  # evaluate
         the_tree.load_model()
