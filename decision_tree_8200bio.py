70b07b1fc194598a598d73987b5290edde9c0d4b
omerugi <omerugi@users.noreply.github.com>
2021-10-01 01:45:47 +0300

    Last min fixs

--- decision_tree_8200bio.py
+++ decision_tree_8200bio.py
@@ -28,6 +28,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.impute import KNNImputer
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn import tree
+
 # from ctgan import CTGANSynthesizer
 # from torch._C import dtype
 # from torch._C import int64
@@ -150,49 +151,31 @@ ord_col = ["location.coverage"
     , "quantity"
            ]
 
-selected_fetuers = ['pain.pain_type', 'location.coverage', 'age', 'duration.days',
-               'quantity', 'topography', 'size', 'is_color_condition_red',
-               'is_color_condition_brown', 'itch', 'swelling',
-               'is_primary_locations_leg', 'lossOfHair.exist', 'is_texture_scales',
-               'is_color_condition_white', 'shape', 'gender',
-               'is_primary_locations_arm', 'is_texture_exfoliation',
-               'is_texture_not_sure', 'is_primary_locations_head', 'is_texture_dry',
-               'is_secondary_locations_neck', 'is_color_condition_normal',
-               'is_primary_locations_back', 'vesicle', 'is_primary_locations_chest',
-               'pain.is_pain', 'is_texture_cracks',
-               'is_secondary_locations_upper_arm_internal',
-               'is_secondary_locations_foot_external', 'is_secondary_locations_toes',
-               'is_color_condition_black', 'is_secondary_locations_nose',
-               'is_secondary_locations_cheeks', 'is_color_condition_purple',
-               'is_secondary_locations_shin_external',
-               'is_secondary_locations_abdomen',
-               'is_secondary_locations_upper_arm_external',
-               'is_secondary_locations_fingers', 'is_texture_wet',
-               'is_secondary_locations_knee', 'is_secondary_locations_navel',
-               'is_secondary_locations_forehead', 'crater',
-               'is_secondary_locations_perioral', 'is_color_condition_blue',
-               'lossOfHair.type', 'is_primary_locations_groin_area',
-               'is_secondary_locations_ear', 'is_nailsdiseases_onychomycosis',
-               'is_secondary_locations_thigh_external', 'is_secondary_locations_chin',
-               'is_secondary_locations_buttock', 'is_secondary_locations_eye',
-               'is_secondary_locations_elbow_pit', 'is_secondary_locations_scrotum',
-               'is_nailsdiseases_pitting', 'is_secondary_locations_kneepit',
-               'is_secondary_locations_back',
-               'is_nailsdiseases_koilonychia__spooning_',
-               'is_secondary_locations_eyelid', 'is_secondary_locations_chest',
-               'is_secondary_locations_hand_internal', 'is_secondary_locations_penis',
-               'is_secondary_locations_scalp', 'is_secondary_locations_thigh_internal',
-               'is_secondary_locations_wrist_internal', 'pus',
-               'is_nailsdiseases_onycholysis', 'bleeding', 'temperature',
-               'duration.from_birth', 'is_texture_rough', 'is_texture_smooth',
-               'is_color_condition_green', 'is_color_condition_grey',
-               'is_color_condition_yellow', 'is_nailsdiseases_beau_s_lines',
-               'is_nailsdiseases_clubbing',
-               'is_nailsdiseases_leukonychia__white_spots_',
-               'is_nailsdiseases_mees__lines', 'is_nailsdiseases_terry_s_nails',
-               'is_nailsdiseases_yellow_nail_syndrome', 'is_primary_locations_buttock',
-               'is_secondary_locations_armpit', 'is_secondary_locations_elbow',
-               'is_secondary_locations_eyebrow']
+rf_selected_fetuers = ['age', 'duration.days', 'size', 'quantity', 'topography', 'is_color_condition_red', 'gender',
+                       'swelling',
+                       'is_color_condition_brown', 'pain.is_pain', 'shape', 'lossOfHair.exist',
+                       'is_primary_locations_leg', 'is_texture_scales',
+                       'is_texture_dry', 'is_texture_exfoliation', 'itch', 'is_primary_locations_head',
+                       'is_color_condition_white',
+                       'pain.pain_type', 'is_primary_locations_chest', 'is_texture_not_sure',
+                       'is_primary_locations_arm',
+                       'is_primary_locations_back', 'is_secondary_locations_thigh_external',
+                       'is_color_condition_normal',
+                       'vesicle', 'location.coverage', 'is_secondary_locations_cheeks',
+                       'is_secondary_locations_fingers',
+                       'is_secondary_locations_upper_arm_internal', 'is_secondary_locations_foot_external',
+                       'is_texture_cracks',
+                       'is_color_condition_purple', 'is_secondary_locations_neck', 'is_secondary_locations_toes',
+                       'is_texture_wet',
+                       'is_color_condition_black', 'is_secondary_locations_hand_internal',
+                       'is_secondary_locations_abdomen',
+                       'is_secondary_locations_elbow_pit', 'is_secondary_locations_perioral',
+                       'is_secondary_locations_nose',
+                       'is_secondary_locations_back', 'is_secondary_locations_upper_arm_external',
+                       'is_secondary_locations_eye',
+                       'is_secondary_locations_forehead', 'lossOfHair.type', 'is_secondary_locations_shin_external',
+                       'is_nailsdiseases_onychomycosis']
+
 
 class DecisionTreeTrainer:
     """ A class for training a decision tree on DermaDetect data """
@@ -258,54 +241,46 @@ class DecisionTreeTrainer:
 
         # location complation
         train = pd.DataFrame(data.to_numpy()[index_train, :], columns=data.columns)
-        x_train = train.drop('diagnosis', axis=1).drop('location.coverage', axis=1).drop('pain.pain_type',axis=1)
+        x_train = train.drop('diagnosis', axis=1).drop('location.coverage', axis=1).drop('pain.pain_type', axis=1)
         y_train = train['location.coverage']
         test = pd.DataFrame(data.to_numpy()[index_test, :], columns=data.columns)
-        x_test = test.drop('diagnosis', axis=1).drop('location.coverage', axis=1).drop('pain.pain_type',axis=1)
+        x_test = test.drop('diagnosis', axis=1).drop('location.coverage', axis=1).drop('pain.pain_type', axis=1)
 
         from sklearn.ensemble import RandomForestClassifier
-        fill_model = RandomForestClassifier(random_state=rng,n_estimators=80)
+        fill_model = RandomForestClassifier(random_state=rng, n_estimators=80)
         fill_model.fit(x_train, y_train)
         y_test = fill_model.predict(x_test)
-        data.loc[index_test,'location.coverage'] = y_test
+        data.loc[index_test, 'location.coverage'] = y_test
 
         # pain completion
         train = pd.DataFrame(data.to_numpy()[index_train_pain, :], columns=data.columns)
-        x_train = train.drop('diagnosis', axis=1).drop('pain.pain_type',axis=1)
+        x_train = train.drop('diagnosis', axis=1).drop('pain.pain_type', axis=1)
         y_train = train['pain.pain_type']
         test = pd.DataFrame(data.to_numpy()[index_test_pain, :], columns=data.columns)
-        x_test = test.drop('diagnosis', axis=1).drop('pain.pain_type',axis=1)
+        x_test = test.drop('diagnosis', axis=1).drop('pain.pain_type', axis=1)
 
-        fill_model = RandomForestClassifier(random_state=rng,n_estimators=90)
+        fill_model = RandomForestClassifier(random_state=rng, n_estimators=90)
         fill_model.fit(x_train, y_train)
         y_test = fill_model.predict(x_test)
-        data.loc[index_test_pain,'pain.pain_type'] = y_test
-
+        data.loc[index_test_pain, 'pain.pain_type'] = y_test
 
         # place holders
         X = data.drop('diagnosis', axis=1)
         y = data['diagnosis']
 
-        # - Need to use only when training model - knn imputer to fill nan columns
+        # - Need to use only when training model - select most important feature
         # - reduce the dimension of the data
-        # imputer = KNNImputer(n_neighbors=17, weights='distance')
-        # X = pd.DataFrame(imputer.fit_transform(X))
-        # X.columns = original.drop('diagnosis', axis=1).columns
-
-        # choose inly imprtent columns - the chosen feature after dimension reduction
-        X = X[selected_fetuers]
-
         # train model to assess feature importance code
         model = tree.DecisionTreeClassifier(random_state=rng)
         model.fit(X, self.OneHot(y))
-        dfscores = pd.DataFrame(model.feature_importances_)
-        dfcolumns = pd.DataFrame(X.columns)
-        featureScores = pd.concat([dfcolumns, dfscores], axis=1)
-        featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
-
-        feture_n = 50
-        X = X[featureScores.nlargest(feture_n, 'Score')['Specs']]
-
+        # dfscores = pd.DataFrame(model.feature_importances_)
+        # dfcolumns = pd.DataFrame(X.columns)
+        # featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+        # featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
+        # feture_n = 50
+        # X = X[featureScores.nlargest(feture_n, 'Score')['Specs']]
+        # print(list(featureScores.nlargest(feture_n, 'Score')['Specs']))
+        X = X[rf_selected_fetuers]
         t = pd.concat([X, y], axis=1)
         return t
 
@@ -328,13 +303,12 @@ class DecisionTreeTrainer:
         ## Simple tree plot
         fig = plt.figure(figsize=(50, 50))
         _ = tree.plot_tree(self.model,
-               class_names=self.target_names,
-               rounded=True,
-               filled = True)
+                           class_names=self.target_names,
+                           rounded=True,
+                           filled=True)
         fig.savefig("decistion_tree.png")
 
         pass
-
 
 
 ################################################################################
@@ -392,10 +366,7 @@ def main():
         test_accuracy_percent = the_tree.evaluate(
             test_data.drop('diagnosis', axis=1), test_data['diagnosis'])
         print("Test accuracy: {:.2f}%".format(test_accuracy_percent))  # in [0-100], eg 99.99%
-        the_tree.visualization( test_data.drop('diagnosis', axis=1), test_data['diagnosis'])
-
-
-
+        the_tree.visualization(test_data.drop('diagnosis', axis=1), test_data['diagnosis'])
 
 
 ################################################################################
